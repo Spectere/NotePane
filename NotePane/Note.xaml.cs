0ad4641083c07b7b14ba79444e45dcec55672160
Ian Burgmyer <spectere@gmail.com>
2016-12-19 17:17:53 -0500

    Notes/tabs can be created.

--- NotePane/Note.xaml.cs
+++ NotePane/Note.xaml.cs
@@ -1,19 +1,27 @@
-﻿using System;
-using System.Windows.Media;
+﻿using System.Windows;
+using System.Windows.Input;
 
 namespace NotePane {
     /// <summary>
     /// Interaction logic for Note.xaml
     /// </summary>
     public partial class Note {
+        private bool _expanded = true;
+
         public Note() {
             InitializeComponent();
 
-            var rng = new Random();
-            var col = new byte[3];
-            rng.NextBytes(col);
-            Background.Background = new SolidColorBrush(Color.FromArgb(byte.MaxValue, col[0], col[1], col[2]));
-            Height = 100;
+            TitleSeparator.Margin = new Thickness(0, TitleRow.Height.Value - 1, 0, 0);
+
+            NoteText.Document.LineHeight = 1;
+            NoteText.AppendText("Hello world!");
+        }
+
+        private void Expand_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            _expanded = !_expanded;
+            Height = _expanded ? double.NaN : TitleRow.Height.Value;
+            Expand.Content = _expanded ? "-" : "+";
+            TitleSeparator.Visibility = _expanded ? Visibility.Hidden : Visibility.Visible;
         }
     }
 }
--- NotePane/NoteView.xaml.cs
+++ NotePane/NoteView.xaml.cs
@@ -1,16 +1,37 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace NotePane {
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class NoteView {
+        private int _tabCount = 0;
+
         public NoteView() {
             InitializeComponent();
+
+            CreateTab();
         }
 
-        private void Add_Click(object sender, RoutedEventArgs e) {
-            NoteStack.Children.Add(new Note());
+        private void AddTab_GotFocus(object sender, RoutedEventArgs e) {
+            CreateTab();
+        }
+
+        private void AddNote_Click(object sender, RoutedEventArgs e) {
+            var activeTab = (TabItem)NoteTabContainer.SelectedItem;
+            var activeNoteContainer = (NoteContainer)activeTab.Content;
+            activeNoteContainer.NoteStack.Children.Add(new Note());
+        }
+
+        private void CreateTab() {
+            var newTab = new TabItem {
+                Header = $"Tab {++_tabCount}",
+                Content = new NoteContainer()
+            };
+
+            NoteTabContainer.Items.Insert(NoteTabContainer.Items.Count - 1, newTab);
+            NoteTabContainer.SelectedItem = newTab;
         }
     }
 }
