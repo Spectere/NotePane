4c7654022ca10b7fc05f3ba0752411c7476b1408
Ian Burgmyer <spectere@gmail.com>
2016-12-19 14:50:06 -0500

    Added test Note control.

---
+++ NotePane/Note.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace NotePane {
+    /// <summary>
+    /// Interaction logic for Note.xaml
+    /// </summary>
+    public partial class Note {
+        public Note() {
+            InitializeComponent();
+
+            var rng = new Random();
+            var col = new byte[3];
+            rng.NextBytes(col);
+            Background.Background = new SolidColorBrush(Color.FromArgb(byte.MaxValue, col[0], col[1], col[2]));
+            Height = 100;
+        }
+    }
+}
--- NotePane/NoteView.xaml.cs
+++ NotePane/NoteView.xaml.cs
@@ -1,10 +1,16 @@
-﻿namespace NotePane {
+﻿using System.Windows;
+
+namespace NotePane {
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class NoteView {
         public NoteView() {
             InitializeComponent();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e) {
+            NoteStack.Children.Add(new Note());
         }
     }
 }
