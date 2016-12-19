62469a97e7f9e59ce90c6003b570fac740952689
Ian Burgmyer <spectere@gmail.com>
2016-12-19 11:57:53 -0500

    Initial (non-MSVC) commit.

---
+++ NotePane/NoteView.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NotePane {
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class NoteView {
+        public NoteView() {
+            InitializeComponent();
+        }
+    }
+}
