6fa12d34ef937a294c941b5426ff4db3308bc872
Ian Burgmyer <spectere@gmail.com>
2016-12-20 15:40:12 -0500

    Tabs can now be renamed.

--- NotePane/NoteView.xaml.cs
+++ NotePane/NoteView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace NotePane {
     /// <summary>
@@ -30,8 +31,44 @@ namespace NotePane {
                 Content = new NoteContainer()
             };
 
+            newTab.MouseDoubleClick += Tab_MouseDoubleClick;
+
             NoteTabContainer.Items.Insert(NoteTabContainer.Items.Count - 1, newTab);
             NoteTabContainer.SelectedItem = newTab;
+        }
+
+        private void Tab_MouseDoubleClick(object sender, RoutedEventArgs e) {
+            var tab = (TabItem)sender;
+            if(tab.Header.GetType() != typeof(string)) return;
+
+            var renameBox = new TextBox {
+                Focusable = true,
+                Tag = tab,
+                Text = tab.Header.ToString()
+            };
+
+            renameBox.LostFocus += (obj, args) => {
+                                       var txtBox = (TextBox)obj;
+                                       var parentTab = (TabItem)txtBox.Tag;
+                                       RenameTab(parentTab, txtBox.Text);
+                                   };
+
+            renameBox.KeyDown += (obj, args) => {
+                                     if(args.Key != Key.Enter || args.Key != Key.Return) return;
+                                     var txtBox = (TextBox)obj;
+                                     var parentTab = (TabItem)txtBox.Tag;
+                                     RenameTab(parentTab, txtBox.Text);
+                                 };
+
+            // Used to set focus when the control becomes visible.
+            renameBox.IsVisibleChanged += (obj, args) => { ((TextBox)obj).Focus(); };
+
+            renameBox.SelectAll();
+            tab.Header = renameBox;
+        }
+
+        private static void RenameTab(TabItem tab, string name) {
+            tab.Header = name;
         }
     }
 }
