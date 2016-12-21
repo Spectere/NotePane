e2e1946d94012f2e73df78a2ba2634e2a0a33149
Ian Burgmyer <spectere@gmail.com>
2016-12-21 03:10:25 -0500

    Removed unused using declarations.

--- NotePane/NoteContainer.xaml.cs
+++ NotePane/NoteContainer.xaml.cs
@@ -1,23 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace NotePane {
+﻿namespace NotePane {
     /// <summary>
     /// Interaction logic for NoteContainer.xaml
     /// </summary>
-    public partial class NoteContainer : UserControl {
+    public partial class NoteContainer {
         public NoteContainer() {
             InitializeComponent();
         }
